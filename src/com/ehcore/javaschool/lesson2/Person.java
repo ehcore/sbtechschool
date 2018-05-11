a69ee33e99d6dbc8e2a7c442507d2a76c6f4d7aa
ehcore <ehcore79@gmail.com>
2018-05-11 14:53:54 +0300

    class Person is ready

--- src/com/ehcore/javaschool/lesson2/Person.java
+++ src/com/ehcore/javaschool/lesson2/Person.java
@@ -11,14 +11,61 @@ public class Person {
         this.name = name;
     }
 
+    public boolean isMan() {
+        return man;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person getSpouse() {
+        return spouse;
+    }
+
+    public void setSpouse(Person spouse){
+        this.spouse = spouse;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Person{")
+          .append("name: ")
+          .append(name)
+          .append(", gender:")
+          .append((man)? "male":"female")
+          .append(", spouse:")
+          .append((spouse != null)? spouse.getName(): "none")
+          .append("}");
+
+        return sb.toString();
+    }
+
     /**
      * This method checks gender of persons. If genders are not equal - tries to marry.
-     * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife. Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
+     * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife.
+     * Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
      * @param person - new husband/wife for this person.
-     * @return - returns true if this person has another gender than passed person and they are not husband and wife, false otherwise
+     * @return - returns true if this person has another gender than passed person and they are not husband and wife,
+     * false otherwise
      */
 
     public boolean marry(Person person){
+        boolean outherIsMan = person.isMan();
+        if(outherIsMan != this.man){
+            if(this.spouse != null){
+                this.spouse.divorce();
+                this.divorce();
+            }
+            if(person.getSpouse() != null){
+                person.getSpouse().divorce();
+                person.divorce();
+            }
+            person.setSpouse(this);
+            this.spouse = person;
+            return true;
+        }
         return false;
     }
 
@@ -28,6 +75,10 @@ public class Person {
      */
 
     public boolean divorce(){
+        if(this.spouse != null){
+            this.spouse = null;
+            return true;
+        }
         return false;
     }
 
