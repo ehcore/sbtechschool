682b49abba3702c9ee62b9cd50fd6f8a6ca24d0c
ehcore <ehcore79@gmail.com>
2018-05-12 17:50:10 +0300

    test commit

--- src/com/ehcore/javaschool/lesson1/HelloWorld.java
+++ src/com/ehcore/javaschool/lesson1/HelloWorld.java
@@ -1,8 +1,5 @@
 package com.ehcore.javaschool.lesson1;
 
-/**
- * Created by user1 on 10.05.2018.
- */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
