c02fe5896b12fa1129b9772bc70c0d8fa0e63200
ehcore <ehcore79@gmail.com>
2018-05-19 20:56:36 +0300

    upd CountMap - add generics

---
+++ src/com/ehcore/javaschool/lesson4/CollectionUtils.java
@@ -0,0 +1,46 @@
+package com.ehcore.javaschool.lesson4;
+
+import java.util.*;
+
+public class CollectionUtils {
+
+    public static <T> void addAll(List<? extends T> source, List<? super T> destination){
+        destination.addAll(source);
+    }
+
+    public static <T> List<T> newArrayList(){
+        return new ArrayList<T>();
+    }
+
+    public static <T> int indexOf(List<T> source, T t){
+        return source.indexOf(t);
+    }
+
+    public static <T> List<? super T> limit(List<? extends T> source, int size){
+
+    }
+
+    public static <T> void add(List<T> source, T t){
+        source.add(t);
+    }
+
+    public static <T> void removeAll(List<T> removeFrom, List<T> c2){
+        removeFrom.removeAll(c2);
+    }
+
+    public static <T> boolean containsAll(List<T> c1, List<T> c2){
+
+    }
+
+    public static <T> boolean containsAny(List<T> c1, List<T> c2){
+
+    }
+
+    public static <T> List<T> range(List<T> list, T min, T max){
+
+    }
+
+    public static <T> List<T> range(List<T> list, T max, Comparator<T> comparator){
+
+    }
+}
