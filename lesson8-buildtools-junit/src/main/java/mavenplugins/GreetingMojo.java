1ad95b9296c2018d1063d8bcd8b0e13f82c69bcd
ehcore <ehcore79@gmail.com>
2018-06-03 20:06:04 +0300

    lesson8

---
+++ lesson8-buildtools-junit/src/main/java/mavenplugins/GreetingMojo.java
@@ -0,0 +1,15 @@
+package mavenplugins;
+
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "sayhi")
+public class GreetingMojo extends AbstractMojo {
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("Hello, dude");
+    }
+}
