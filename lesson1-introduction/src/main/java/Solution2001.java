9d3bd21f6c2e2208f315b4c84a61f9338bdc8d4d
ehcore <ehcore79@gmail.com>
2018-06-08 11:12:59 +0300

    upd

--- lesson1-introduction/src/main/java/Solution2001.java
+++ lesson1-introduction/src/main/java/Solution2001.java
@@ -1,5 +1,19 @@
 import java.io.*;
 
+/**
+ * 2001. A+B
+ * http://acm.sgu.ru/lang/problem.php?contest=2&problem=2001
+ *
+ * Заданы a и b. Выведите a+b.
+ *
+ * Входные данные
+ * В единственной строке входных данных заданы целочисленные a и b (1 ≤ a,b ≤ 1000).
+ *
+ * Выходные данные
+ * Выведите a+b.
+ *
+ */
+
 public class Solution2001 {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
--- lesson1-introduction/src/main/java/Solution2002.java
+++ lesson1-introduction/src/main/java/Solution2002.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 
+/**
+ * 2002. Сумма чисел
+ * http://acm.sgu.ru/lang/problem.php?contest=2&problem=2002
+ *
+ * Задано n целых чисел. Выведите их сумму.
+ *
+ * Входные данные
+ * В первой строке задано целое число n (1 ≤ n ≤ 10_000).
+ * Вторая строка содержит n целых чисел, каждое от 1 до 10_000, включительно.
+ *
+ * Выходные данные
+ * Выведите искомую сумму.
+ *
+ */
+
 public class Solution2002 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
