7134002a69abce5c7958c8e88ca5752861d1b3c9
ehcore <ehcore79@gmail.com>
2018-06-09 20:59:25 +0300

    lesson8

--- lesson10-java8-lambda-streamapi/src/main/java/Main.java
+++ lesson10-java8-lambda-streamapi/src/main/java/Main.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
+        Arrays.asList("aa","bb","cc").stream()
+                .filter(s -> "cc".equals(s))
+                .forEach(System.out::println);
 
     }
 }
