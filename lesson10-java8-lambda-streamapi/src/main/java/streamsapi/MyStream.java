911a7f82a9588145d8d939858e0fb2ecfe43ee75
ehcore <ehcore79@gmail.com>
2018-06-13 16:40:33 +0300

    upd stream api

--- lesson10-java8-lambda-streamapi/src/main/java/streamsapi/MyStream.java
+++ lesson10-java8-lambda-streamapi/src/main/java/streamsapi/MyStream.java
@@ -1,14 +1,53 @@
 package streamsapi;
 
 import java.util.*;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.function.*;
 
-public interface MyStream<T> {
+/**
+ * Пока сырой код, все плохо и ничего не работает
+ * @param <T> - даже и не известно, что здесь будет
+ */
+
+public class   MyStream<T> {
+    private List<T> collection;
+    private Queue<Process> queueTasks = new LinkedList<>();
+
+    MyStream(List<T> collection){
+        this.collection = collection;
+    }
+
+    static <T> MyStream of(List<T> collection){
+        List<T> newCollection = new ArrayList();
+        newCollection.addAll(collection);
+        return new MyStream(newCollection);
+    }
+
+    MyStream filter(Predicate predicate){
+        Filter filter = new Filter();
+        queueTasks.add(filter);
+        return this;
+    }
+
+    List<T> collect(){
+        for(Process task: queueTasks){
+            task.doWork();
+        }
+        return collection;
+    }
+
+    private class Filter implements Process{
+        @Override
+        public void doWork() {
+
+        }
+    }
+
+    /*
     static MyStream of(Collection collection){
         return null;
     }
     MyStream<T> filter(Predicate<T> predicate);
     <R> MyStream<R> transform(Function<T,R> function);
     Map toMap();
+    */
 }
---
+++ lesson10-java8-lambda-streamapi/src/main/java/streamsapi/Process.java
@@ -0,0 +1,5 @@
+package streamsapi;
+
+public interface Process {
+    void doWork();
+}
