139a36ce15c45ab96c44fac945c286525c8c2e0e
ehcore <ehcore79@gmail.com>
2018-07-10 15:17:22 +0300

    refactor

--- lesson17-goodcode/src/main/java/ru/sbt/bit/ood/solid/homework/SalaryHtmlReportNotifier.java
+++ lesson17-goodcode/src/main/java/ru/sbt/bit/ood/solid/homework/SalaryHtmlReportNotifier.java
@@ -13,15 +13,18 @@ import java.sql.SQLException;
 public class SalaryHtmlReportNotifier {
 
     private Connection connection;
+    private final String SQL = "select emp.id as emp_id, emp.name as amp_name, sum(salary) as salary from employee emp left join" +
+            "salary_payments sp on emp.id = sp.employee_id where emp.department_id = ? and" +
+            " sp.date >= ? and sp.date <= ? group by emp.id, emp.name";
 
     public SalaryHtmlReportNotifier(Connection databaseConnection) {
         this.connection = databaseConnection;
     }
 
-    public void generateAndSendHtmlSalaryReport(String departmentId, DateRange dateRange, String recipients) {
+    public void /*generateAndSendHtml*/ makeSalaryReport(String departmentId, DateRange dateRange, String recipients) {
         try {
-            ResultSet results = executeQuery(departmentId,dateRange);
-            StringBuilder resultingHtml = generateHtml(results);
+            ResultSet results = getQueryResult(departmentId,dateRange);
+            StringBuilder resultingHtml = getHtml(results);
             sendReport(resultingHtml,recipients);
         } catch (SQLException e) {
             e.printStackTrace();
@@ -30,11 +33,9 @@ public class SalaryHtmlReportNotifier {
         }
     }
 
-    private ResultSet executeQuery(String departmentId, DateRange dateRange)throws SQLException{
+    private ResultSet getQueryResult(String departmentId, DateRange dateRange)throws SQLException{
         // prepare statement with sql
-        PreparedStatement ps = connection.prepareStatement("select emp.id as emp_id, emp.name as amp_name, sum(salary) as salary from employee emp left join" +
-                "salary_payments sp on emp.id = sp.employee_id where emp.department_id = ? and" +
-                " sp.date >= ? and sp.date <= ? group by emp.id, emp.name");
+        PreparedStatement ps = connection.prepareStatement(SQL);
         // inject parameters to sql
         ps.setString(1, departmentId);
         ps.setDate(2, new java.sql.Date(dateRange.getDateFrom().toEpochDay()));
@@ -44,7 +45,7 @@ public class SalaryHtmlReportNotifier {
         return results;
     }
 
-    private StringBuilder generateHtml(ResultSet results) throws SQLException{
+    private StringBuilder getHtml(ResultSet results) throws SQLException{
         // create a StringBuilder holding a resulting html
         StringBuilder resultingHtml = new StringBuilder();
         resultingHtml.append("<html><body><table><tr><td>Employee</td><td>Salary</td></tr>");
