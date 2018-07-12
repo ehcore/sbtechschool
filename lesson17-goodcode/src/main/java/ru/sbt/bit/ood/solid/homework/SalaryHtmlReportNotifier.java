03ac31c8f2d483bf659a3f70e3d18d543fd596d8
ehcore <ehcore79@gmail.com>
2018-07-12 10:47:03 +0300

    refactor

--- lesson17-goodcode/src/main/java/ru/sbt/bit/ood/solid/homework/SalaryHtmlReportNotifier.java
+++ lesson17-goodcode/src/main/java/ru/sbt/bit/ood/solid/homework/SalaryHtmlReportNotifier.java
@@ -14,7 +14,7 @@ public class SalaryHtmlReportNotifier {
 
     private Connection connection;
     private final String SQL = "select emp.id as emp_id, emp.name as amp_name, sum(salary) as salary from employee emp left join" +
-            "salary_payments sp on emp.id = sp.employee_id where emp.department_id = ? and" +
+            " salary_payments sp on emp.id = sp.employee_id where emp.department_id = ? and" +
             " sp.date >= ? and sp.date <= ? group by emp.id, emp.name";
 
     public SalaryHtmlReportNotifier(Connection databaseConnection) {
