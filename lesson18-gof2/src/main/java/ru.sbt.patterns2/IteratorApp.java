94872a6c28081701dbffc585d79259d84dfe1746
ehcore <ehcore79@gmail.com>
2018-07-13 13:02:32 +0300

    upd gof2

---
+++ lesson18-gof2/src/main/java/ru.sbt.patterns2/IteratorApp.java
@@ -0,0 +1,51 @@
+package ru.sbt.patterns2;
+
+
+public class IteratorApp {
+    public static void main(String[] args) {
+        MyCollection c = new MyCollection();
+
+        Iterator it = c.getIterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
+
+interface Iterator {
+    Object next();
+
+    boolean hasNext();
+}
+
+interface Container {
+    Iterator getIterator();
+}
+
+class MyCollection implements Container {
+    private static final String[] collect = {"Картофель", "Свекла", "Капуста", "Лук", "Томат", "Мясо"};
+
+    @Override
+    public Iterator getIterator() {
+        return new MyIterator();
+    }
+
+    private class MyIterator implements Iterator {
+        int index = 0;
+
+        @Override
+        public Object next() {
+            return collect[index++];
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (index < collect.length) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
