12d28c4721ab7c50542db8b5f2354f24d7b65b89
ehcore <ehcore79@gmail.com>
2018-07-17 11:26:05 +0300

    create gof1

--- lesson19-gof1/src/main/java/ru/sbt/patterns1/creational/FactoryMethodApp.java
+++ lesson19-gof1/src/main/java/ru/sbt/patterns1/creational/FactoryMethodApp.java
@@ -3,10 +3,20 @@ package ru.sbt.patterns1.creational;
 
 public class FactoryMethodApp {
     public static void main(String[] args) {
-
+        WatchMaker maker = getMaker("Digital");
+        Watch watch = maker.createWatch();
+        watch.showTime();
+    }
+    static WatchMaker getMaker(String maker){
+        if("Digital".equals(maker)){
+            return DigitalWatch::new;
+        }else if("Romain".equals(maker)){
+            return RomainWatch::new;
+        }else{
+            throw new UnsupportedOperationException("not know watch: " + maker);
+        }
     }
 }
-
 
 interface Watch{
     void showTime();
@@ -23,5 +33,23 @@ class RomainWatch implements Watch{
     @Override
     public void showTime() {
         System.out.println("romain watch");
+    }
+}
+
+interface WatchMaker{
+    Watch createWatch();
+}
+
+class DigitalWatchMaker implements WatchMaker{
+    @Override
+    public Watch createWatch() {
+        return new DigitalWatch();
+    }
+}
+
+class RomainWatchMaker implements WatchMaker{
+    @Override
+    public Watch createWatch() {
+        return new RomainWatch();
     }
 }
