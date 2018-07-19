30a8ba7ab93509e7bf218302ae695f8f05a19106
ehcore <ehcore79@gmail.com>
2018-07-19 10:24:14 +0300

    create gof1

--- lesson19-gof1/src/main/java/ru/sbt/patterns1/creational/SingletonApp.java
+++ lesson19-gof1/src/main/java/ru/sbt/patterns1/creational/SingletonApp.java
@@ -4,5 +4,41 @@ package ru.sbt.patterns1.creational;
 public class SingletonApp {
     public static void main(String[] args) {
 
+        Thread t[] = new Thread[1000];
+
+        for (int i = 0; i < t.length; i++) {
+            t[i] = new Thread(Singleton::getInstance);
+            t[i].start();
+        }
+
+        for (int i = 0; i < t.length; i++) {
+            try {
+                t[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(Singleton.count);
     }
+}
+
+class Singleton {
+    private static volatile Singleton instance;
+    public static int count;
+
+    private Singleton() {
+        count++;
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null)
+                    instance = new Singleton();
+            }
+        }
+        return instance;
+    }
+
 }
